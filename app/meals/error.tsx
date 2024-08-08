ebf60cf3ef4c0a6d27107418674dc485e73ed4f7
Jesus Rodriguez <jesusrod789@gmail.com>
2024-08-08 13:01:25 -0700

    global not-found.tsx page added

--- app/meals/error.tsx
+++ app/meals/error.tsx
@@ -3,7 +3,7 @@
 export default function Error() {
   return (
     <main className="mt-[5rem] text-center ">
-      <h1 className="text-[5rem] m-0 font-black text-[#262626] uppercase bg-cover bg-center font-monsterrat highlight ">
+      <h1 className="text-[5rem] m-0 font-black text-[#262626] uppercase bg-cover bg-center font-monsterrat highlight">
         An error occurred!
       </h1>
       <p className="text-[1.5rem] font-medium  text-[#ddd8d8]">
